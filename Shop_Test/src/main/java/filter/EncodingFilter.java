62db2633edc705c773de897400dc6e01deba0836
Lee-0210 <tjsans9069@gmail.com>
2025-05-28 23:11:38 +0900

    초기 커밋

--- Shop_Test/src/main/java/filter/EncodingFilter.java
+++ Shop_Test/src/main/java/filter/EncodingFilter.java
@@ -2,16 +2,22 @@ package filter;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
 public class EncodingFilter implements Filter {
 	
-	private String encoding; 
+	private String encoding;
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	} 
 	
 }
 
