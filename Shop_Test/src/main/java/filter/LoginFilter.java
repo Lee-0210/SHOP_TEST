50f2077a1a6e3c2ae0026629550f586d8dcfab7c
Lee-0210 <tjsans9069@gmail.com>
2025-06-03 00:43:00 +0900

    합치기

--- Shop_Test/src/main/java/filter/LoginFilter.java
+++ Shop_Test/src/main/java/filter/LoginFilter.java
@@ -1,8 +1,17 @@
 package filter;
 
+import java.io.IOException;
+
 import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet Filter implementation class LoginFilter
@@ -10,5 +19,32 @@ import jakarta.servlet.http.HttpFilter;
 @WebFilter("/LoginFilter")
 public class LoginFilter extends HttpFilter implements Filter {
 	
-	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+
+		
+		Cookie[] cookies = req.getCookies();
+		String rememberMe = null;
+		String token = null;
+		if(cookies != null) {
+			for(Cookie cookie : cookies) {
+				if(cookie.getName().equals("rememberMe")) {
+					rememberMe = cookie.getValue();
+					break;
+				}
+				if(cookie.getName().equals("token")) {
+					token = cookie.getValue();
+				}
+			}
+		}
+		
+		
+		
+		
+		
+		
+		chain.doFilter(req, res);
+	}
 }
